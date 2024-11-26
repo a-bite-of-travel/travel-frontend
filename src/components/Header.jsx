b0b19476a08090106619f934208f6d9c115cc656
jijayoung <youhere@gmail.com>
2024-11-26 11:31:15 +0900

    feat:header수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,5 @@
-import { Stack, Container, Button, Grid,
-    Avatar,  Typography } from '@mui/material'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
-
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#0082FF', //blue
-        },
-        secondary: {
-            main: '#FF0A73', //pink
-        },
-        yellow: {
-            main: '#FABF13',
-        },
-        black: {
-            main: '#333',
-        },
-        darkGray: {
-            main: '#888',
-        },
-        lightGray: {
-            main: '#D9D9D9',
-        },
-    },
-});
+import { Stack, Container, Button, Grid, Avatar, Typography, Link } from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom';
 
 export default function Header() {
     return (
@@ -32,30 +7,29 @@ export default function Header() {
             <header>
                 <Container maxWidth="lg">
                     <Grid container>
-                        <Grid item xs sx={{display: 'flex', alignItems: 'center'}}>
-                            <h1><LinkTo to="/tour"><img src="images/h1_ssoulRoad.png" alt="Ssoul Road"/></LinkTo></h1>
+                        <Grid item xs sx={{ display: 'flex', alignItems: 'center' }}>
+                            <h1><Link component={RouterLink} to="/tour"><img src="images/h1_ssoulRoad.png" alt="Ssoul Road" /></Link></h1>
                         </Grid>
-                        <Grid item sx={{display: 'flex', alignItems: 'center'}} className="nav">
+                        <Grid item sx={{ display: 'flex', alignItems: 'center' }} className="nav">
                             <Stack spacing={10} direction="row">
-                                <Link to={} color="inherit" underline="none"
-                                      sx={{'&:hover': {color: 'primary.main'}}}><b>여행계획</b></Link>
-                                <Link to={} color="inherit" underline="none"
-                                      sx={{'&:hover': {color: 'primary.main'}}}><b>여행정보</b></Link>
+                                <Link component={RouterLink} to="/" color="inherit" underline="none" sx={{ '&:hover': { color: 'primary.main' } }}><b>여행계획</b></Link>
+                                <Link component={RouterLink} to="/" color="inherit" underline="none" sx={{ '&:hover': { color: 'primary.main' } }}><b>여행정보</b></Link>
+                                <Link component={RouterLink} to="/guide" color="inherit" underline="none" sx={{ '&:hover': { color: 'primary.main' } }}><b>Guide</b></Link>       
                             </Stack>
                         </Grid>
-                        <Grid item sx={{display: 'flex', alignItems: 'center'}}>
+                        <Grid item sx={{ display: 'flex', alignItems: 'center' }}>
                             <Stack spacing={1} direction="row" justifyContent="flex-end">
-                                <Button variant="text" size="small" sx={{borderRadius: '20px'}}>Login</Button>
-                                <Button variant="contained" sx={{borderRadius: '20px'}} size="small">Sign up</Button>
+                                <Button variant="text" size="small" sx={{ borderRadius: '20px' }}>Login</Button>
+                                <Button variant="contained" sx={{ borderRadius: '20px' }} size="small">Sign up</Button>
                             </Stack>
-                            <Stack direction="row" sx={{alignItems: 'center'}} justifyContent="flex-end">
-                                <Link color="inherit" underline="none" href="#"
-                                      sx={{'&:hover': {color: 'primary.main'}, display: 'flex', alignItems: 'center'}}>
-                                    <Avatar alt="Default" src="images/profile_default.png"/>
+                            <Stack direction="row" sx={{ alignItems: 'center' }} justifyContent="flex-end">
+                                <Link color="inherit" underline="none" to="/" component={RouterLink}
+                                    sx={{ '&:hover': { color: 'primary.main' }, display: 'flex', alignItems: 'center' }}>
+                                    <Avatar alt="Default" src="images/profile_default.png" />
                                     <Typography variant="body1">김쏘울</Typography>
                                 </Link>
                                 <Button variant="text" size="small"
-                                        sx={{borderRadius: '20px', marginLeft: '20px !important'}}>Sign out</Button>
+                                    sx={{ borderRadius: '20px', marginLeft: '20px !important' }}>Sign out</Button>
                             </Stack>
                         </Grid>
                     </Grid>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,38 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import router from './routes/route';
-import {RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0082FF', //blue
+    },
+    secondary: {
+      main: '#FF0A73', //pink
+    },
+    yellow: {
+      main: '#FABF13',
+    },
+    black: {
+      main: '#333',
+    },
+    darkGray: {
+      main: '#888',
+    },
+    lightGray: {
+      main: '#D9D9D9',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
