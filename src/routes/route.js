2186386d433eceedd2b028397b96a5a936af16a2
hhj08 <hhhj.1486@gmail.com>
2024-11-28 16:22:07 +0900

    feat: 여행정보 조회

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ import RouterLayout from './RouterLayout';
 import Main from './Main';
 import Guide from '../components/Guide';
 import Review from '../routes/Review'
+import TourInfo, {loader as tourInfoLoader } from './TourInfo';
 
 const router = createBrowserRouter([
     {
@@ -11,8 +12,10 @@ const router = createBrowserRouter([
         children: [
             {path: '/', element: <Main />}, // 매인
             {path: '/:userId', element: <Main />}, // 마이페이지
-            {path: '/tour', element: <Main />}, // 여행 게획
-            {path: '/tourinfo', element: <Main />}, // 여행 정보
+            {path: '/plan', element: <Main />}, // 여행 게획
+            {path: '/tour', element: <TourInfo />, loader: (args) => tourInfoLoader({ ...args, type: "관광지" }),}, // 여행 정보
+            {path: '/food', element: <TourInfo />, loader: (args) => tourInfoLoader({ ...args, type: "음식점" })},
+            {path: '/festival', element: <TourInfo />, loader: (args) => tourInfoLoader({ ...args, type: "축제" })},
             {path: '/review', element: <Review />}, // 리뷰
             {path: '/guide', element: <Guide />}, // 임시메뉴 Guide
         ]
