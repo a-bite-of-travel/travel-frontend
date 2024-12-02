35863e87b48e3ee30f42c100ee44ba2dc6ee1f83
ejkim1051 <ejkim1029@hanmail.net>
2024-12-02 11:05:12 +0900

    style:컴포넌트명수정

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,25 +2,33 @@ import {createBrowserRouter} from 'react-router-dom';
 import RouterLayout from './RouterLayout';
 import Main from './Main';
 import Guide from '../components/Guide';
-import Tourinfo from './tourinfo';
-import Dining from './Dining';
-import Fiesta from './Fiesta';
+import Login from './Login';
+import Signup from './Signup';
+import Mypage from './Mypage';
+import ReviewTour from './ReviewTour';
 import Review from './Review';
+// import TourInfo, { loader as tourInfoLoader } from './TourInfo';
+import Location from './location';
 
 const router = createBrowserRouter([
     {
         path: '',
         element: <RouterLayout />,
         children: [
-            {path: '/', element: <Main />}, // 매인
-            {path: '/:userId', element: <Main />}, // 마이페이지
-            {path: '/tour', element: <Main />}, // 여행 게획
+            {path: '/', element: <Main />}, // 메인
+            {path: '/:userId', element: <Mypage />}, // 마이페이지
             {path: '/tourinfo', element: <Tourinfo />}, // 여행 정보 // 여행지
-            {path: '/dining', element: <Dining />}, // 음식점
-            {path: '/fiesta', element: <Fiesta />}, // 축제
+            {path: '/plan', element: <Main />}, // 여행 게획
+            // {path: '/tour', element: <TourInfo />, loader: (args) => tourInfoLoader({ ...args, type: "관광지" }),}, // 여행 정보
+            // {path: '/food', element: <TourInfo />, loader: (args) => tourInfoLoader({ ...args, type: "음식점" })},
+            // {path: '/festival', element: <TourInfo />, loader: (args) => tourInfoLoader({ ...args, type: "축제" })},
+            // {path: '/location', element: <Location />}, // 여행지
+            // {path: '/dining', element: <Dining />}, // 음식점
+            // {path: '/fiesta', element: <Fiesta />}, // 축제
             {path: '/review', element: <Review />}, // 후기작성
             {path: '/guide', element: <Guide />}, // 임시메뉴 Guide
-            
+            {path: '/auth/login', element: <Login />}, // 로그인
+            {path: '/auth/Signup', element: <Signup />}, // 회원가입
         ]
     },
 ]);
